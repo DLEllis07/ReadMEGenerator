e37539eccbb2ecf72f3982270ffec589c9cebfe8
Damar Ellis <damar.ellis22@gmail.com>
2023-05-25 15:08:47 -0400

    Added Utils folder

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,6 @@
+function renderLicenseBadge(license) {
+    if (license === "MIT") {
+        return "![License:MIT](https://img.shields.io/badge/License-MIT-yellow.svg)";
+    } else if (license === "Apache") {
+        return "![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)";
+    }}
