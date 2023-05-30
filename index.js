b51e77716902af0730ed2feef4e4d694bf976f3b
Damar Ellis <damar.ellis22@gmail.com>
2023-05-29 23:32:46 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+ const inquirer = require('inquirer');
+ const fs = require(fs);
+ const generateMarkdown = require("./utils/generateMarkdown");
+
+ const questions = [
+{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Briefly, describe what your project is about',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'How will you install your project?',
+},
+{
+    type: 'input',
+    name: 'use',
+    message: 'How will you use your project?', 
+},
+{
+    type: 'list',
+    name: 'installation',
+    message: 'enter the license chosen for the repo',
+    choices:[
+        'MIT', 'Apache', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None',
+      ]
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'Enter your github username',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter your user email',
+},
+ ];
+
+ function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, 'utf8', (err) =>
+    err ? console.error(err) : console.log('Success')
+  );
+ }
